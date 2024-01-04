c3e9ee3c55695de7dc4b49f8d6db72bcf3e1fd57
joshmu12 <146480161+joshmu12@users.noreply.github.com>
2024-01-04 16:55:53 -0500

    Add files via upload

--- homepage.py
+++ homepage.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import pandas as pd
 
+# web scraping 
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+response = requests.get(url)
+
+if response.status_code == 200: 
+
+        data = BeautifulSoup(response.text , 'html')
+        print(data)
+
 from streamlit_option_menu import option_menu
 
 df = pd.read_csv('fplAnalytics-playerStautsData (1).csv')
@@ -14,6 +26,8 @@ st.set_page_config(
 def team_page(): #team page define 
         st.title("Teams")
 
+def main():
+        st.title('Players')
 
 # sidebar 
 
