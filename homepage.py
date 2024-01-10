6323b22659dad331357a7ea5fd9d2e7f1c119d73
joshmu12 <146480161+joshmu12@users.noreply.github.com>
2024-01-09 22:26:47 -0500

    Add files via upload

--- homepage.py
+++ homepage.py
@@ -6,6 +6,7 @@ from PIL import Image
 
 import requests
 import json
+import plotly.express as px 
 
 stats_url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 
@@ -32,6 +33,11 @@ if response.status_code == 200:
         for teams in fpl_data["teams"]:
                 teams_list.append(teams['name'])
 
+        # short form 
+        short_teams_list = []
+        for teams in fpl_data["teams"]:
+                short_teams_list.append(teams['short_name'])
+
 else:
     print(f"Failed to retrieve FPL data. Status code: {response.status_code}")
 
@@ -42,21 +48,34 @@ st.set_page_config(
 )
 
 # sidebar 
+
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
         selected = option_menu(
                 menu_title = "Fantasy Premier League",
-                options = ["Home" , "Players", "Teams"],
-                icons = ["house","person", "people"]
+                options = ["Home" , "Players Detailed", "Teams / Fixtures", "Player Rankings", "Team Builder"],
+                icons = ["house","person", "people", "bar-chart-fill", "cone-striped"]
         )
 
 if selected == "Home":
         st.title("Fantasy Premier League Tracker")
 
+# position getter function 
+def position_getter(num):
+        if num == 1:
+                return "Goalkeeper"
+        elif num == 2:
+                return "Defender"
+        elif num == 3:
+                return "Midfielder"
+        else:
+                return "Forward"
+
+
 # players stats search 
 
-if selected == "Players":
+if selected == "Players Detailed":
         st.title("Players Search")
         
 
@@ -66,6 +85,7 @@ if selected == "Players":
         #find player num in list
         searched_player_num = ''
         temp = 0
+
         for player in unsorted_player_list:
                 if player == player_searched:
                         searched_player_num = temp
@@ -99,17 +119,6 @@ if selected == "Players":
 
                 st.image(player_image_url, width = 140)
 
-        # position getter function 
-        def position_getter(num):
-                if num == 1:
-                        return "Goalkeeper"
-                elif num == 2:
-                        return "Defender"
-                elif num == 3:
-                        return "Midfielder"
-                else:
-                        return "Forward"
-
         #visualise details
         with colA2: 
                 st.subheader(unsorted_player_list[searched_player_num]) # name
@@ -139,15 +148,243 @@ if selected == "Players":
         #points per game 
         colB3.metric("Points Per Game", player_stats[searched_player_num]['points_per_game'])
 
-        # visualise overall data
+        # points graph 
+        player_points_overall_data = {}
+        player_opponent_list = []
+        player_points_list = []
+        gameweek_count = []
+        temp = 1
 
-        #player_overall_data = player_stats[searched_player_num]
+        for gameweek in individual_player_match_data: 
+                player_opponent_list.append(short_teams_list[gameweek['opponent_team'] - 1])
+                player_points_list.append(gameweek['total_points'])
+                gameweek_count.append(gameweek['round'])
+                temp += 1 
 
-        #player_overall_df = pd.DataFrame(player_overall_data, index=[0])
-        #player_overall_df = player_overall_df.T
+        player_points_overall_data = { 
+                'Opponents' : player_opponent_list,
+                'Points' : player_points_list,
+                'Gameweek' : gameweek_count
+        }
 
-        #st.write(player_overall_df)
+        individual_points_df = pd.DataFrame(player_points_overall_data)
+        player_points_fig = px.scatter(individual_points_df, x = 'Gameweek', y = 'Points', text = 'Opponents', title = 'Points Change Graph')
+        player_points_fig.add_trace(px.line(individual_points_df, x = 'Gameweek', y = 'Points').data[0])
+        st.plotly_chart(player_points_fig)
+
+        # price graph 
+        player_price_overall_data = {}
+        player_price_list = []
+        temp = 0
+
+        for gameweek in individual_player_match_data:
+                temp = gameweek['value']
+                temp = temp / 10 
+                player_price_list.append(temp)
+
+        player_price_overall_data = { 
+                'Gameweek' : gameweek_count,
+                'Price' : player_price_list
+        }
+
+        individual_price_df = pd.DataFrame(player_price_overall_data)
+        player_price_fig = px.scatter(individual_price_df, x = 'Gameweek', y = 'Price', title = 'Price Change Graph')
+        player_price_fig.add_trace(px.line(individual_price_df, x = 'Gameweek', y = 'Price').data[0])
+        st.plotly_chart(player_price_fig)
+
+        # selection graph 
+        player_selection_overall_data = {}
+        player_selection_list = []
         
+        for gameweek in individual_player_match_data:
+                player_selection_list.append(gameweek['selected'])
+
+        player_selection_overall_data = {
+                'Selected' : player_selection_list,
+                'Gameweek': gameweek_count
+        }
+
+        individual_selection_df = pd.DataFrame(player_selection_overall_data)
+        player_selection_fig = px.scatter(individual_selection_df, x = 'Gameweek', y = 'Selected', title = 'Selected by Users Number Graph')
+        player_selection_fig.add_trace(px.line(individual_selection_df, x = 'Gameweek', y = 'Selected').data[0])
+        st.plotly_chart(player_selection_fig)
+
+
+if selected == "Teams / Fixtures":
+        st.title ("Teams List")
+
+        # Club Picker
+        teams_searched = st.selectbox("Enter Club name:", teams_list)
+
+        club_num = teams_list.index(teams_searched)
+        team_data = fpl_data['teams'][club_num]
+
+        #find team num in list
+        colC1, colC2  = st.columns ((6,4))
+        with colC1:
+                team_code = team_data['code']
+        
+                team_image_url = f"https://resources.premierleague.com/premierleague/badges/100/t{team_code}.png"
+
+                st.image(team_image_url, width = 150)
+
+        with colC2 :
+                st.title(team_data['name'])
+        
+        st.title('')
+
+        st.subheader(f"{team_data['name']} Players on Top Form")
+        st.title('')
+
+        team_detailed_data = {}
+
+        for players in player_stats:
+
+                if players['team_code'] == team_code:
+
+                        team_detailed_data [players['first_name'] + " " + players['second_name']] = players
+
+        sorted_team_detailed_form_data = dict(sorted(team_detailed_data.items(), key=lambda item: item[1]['form'], reverse=True))
+
+        # get players code
+
+        sorted_player_key_list = list(sorted_team_detailed_form_data.keys())
+
+        first_player = sorted_player_key_list[0]
+        second_player = sorted_player_key_list[1]
+        third_player = sorted_player_key_list[2]
+
+        # first player 
+
+        colD1, colD2, colD3  = st.columns ((2,4,4))
+
+        # first player image 
+        with colD1:
+                st.subheader('1.')
+        with colD2:
+
+                first_player_image_url = f"https://resources.premierleague.com/premierleague/photos/players/110x140/p{sorted_team_detailed_form_data[first_player]['code']}.png"
+
+                st.image(first_player_image_url, width = 180)
+
+        # first player details 
+        with colD3: 
+                st.subheader(sorted_team_detailed_form_data[first_player]['first_name'] + " " + sorted_team_detailed_form_data[first_player]['second_name']) # name
+                st.write(f"Position: {position_getter(sorted_team_detailed_form_data[first_player]['element_type'])}") #positon
+                st.write(f"Form: {sorted_team_detailed_form_data[first_player]['form']}")
+                st.write(f"Price: {sorted_team_detailed_form_data[first_player]['now_cost'] / 10}")
+
+        # second player 
+
+        st.title('')
+
+        colE1, colE2, colE3  = st.columns ((2,4,4))
+
+        # second player image 
+        with colE1:
+                st.subheader('2.')
+        with colE2:
+
+                second_player_image_url = f"https://resources.premierleague.com/premierleague/photos/players/110x140/p{sorted_team_detailed_form_data[second_player]['code']}.png"
+
+                st.image(second_player_image_url, width = 180)
+
+        # second player details 
+        with colE3: 
+                st.subheader(sorted_team_detailed_form_data[second_player]['first_name'] + " " + sorted_team_detailed_form_data[second_player]['second_name']) # name
+                st.write(f"Position: {position_getter(sorted_team_detailed_form_data[second_player]['element_type'])}") #positon
+                st.write(f"Form: {sorted_team_detailed_form_data[second_player]['form']}")
+                st.write(f"Price: {sorted_team_detailed_form_data[second_player]['now_cost'] / 10}")
+
+        # third player 
+
+        st.title('')
+
+        colF1, colF2, colF3  = st.columns ((2,4,4))
+
+        # third player image 
+        with colF1:
+                st.subheader('3.')
+        with colF2:
+
+                third_player_image_url = f"https://resources.premierleague.com/premierleague/photos/players/110x140/p{sorted_team_detailed_form_data[third_player]['code']}.png"
+
+                st.image(third_player_image_url, width = 180)
+
+        # second player details 
+        with colF3: 
+                st.subheader(sorted_team_detailed_form_data[third_player]['first_name'] + " " + sorted_team_detailed_form_data[third_player]['second_name']) # name
+                st.write(f"Position: {position_getter(sorted_team_detailed_form_data[third_player]['element_type'])}") #positon
+                st.write(f"Form: {sorted_team_detailed_form_data[third_player]['form']}")
+                st.write(f"Price: {sorted_team_detailed_form_data[third_player]['now_cost'] / 10}")
+
+        st.title('')
+
+        # fixtures
+
+        st.subheader(f"{team_data['name']} Fixtures")
+
+        fixtures_url = "https://fantasy.premierleague.com/api/fixtures/?future=1" # transfer the data
+
+        fixtures_response = requests.get(fixtures_url)
+
+        if response.status_code == 200:
+
+                fixtures_data = json.loads(fixtures_response.text) # Parse the JSON content of the API response
+
+        else:
+                print(f"Failed to retrieve FPL data. Status code: {response.status_code}")
+
+        overall_team_fixtures_data = {}
+
+        team_id = team_data['id']
+
+        for match in fixtures_data:
+                if match['team_a'] == team_id or match['team_h'] == team_id:
+                        overall_team_fixtures_data[match['event']] = match
+
+        def home_away(dict, team_id): # get home or away
+                if dict['team_a'] == team_id:
+                        return 'Away'
+                else:
+                        return 'Home'
+
+        
+        def opponent_team_getter(dict, team_id): # get opponenet team
+                if dict['team_a'] == team_id:
+                        return dict['team_h']
+                else:
+                        return dict['team_a']
+
+        def opponent_team_level_getter(dict, team_id): # get opponenet team level
+                if dict['team_a'] == team_id:
+                        return dict['team_h_difficulty']
+                else:
+                        return dict['team_a_difficulty']
+
+        filter_dict = {}
+
+        for match, data in overall_team_fixtures_data.items():
+                
+                filter_dict[data['event']] = {
+                        'Opponent' : teams_list[opponent_team_getter(data, team_id) - 1],
+                        'Home / Away' : home_away(data, team_id),
+                        'Diffuculty Level' : (opponent_team_level_getter(data, team_id))
+                 }
+
+        print(filter_dict)
+                
+
+        
+
+
+
+        
+
+
+        
+
+
 
 
         
